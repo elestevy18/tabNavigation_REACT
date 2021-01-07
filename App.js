968910c049a2989c42d233650e23750c40e3b21b
Kevin Jimenez <53486006+elestevy18@users.noreply.github.com>
2021-01-07 06:45:42 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import LandingScreen from './screens/LandingScreen';
+
+import 'react-native-gesture-handler';
+
+const Drawer = createDrawerNavigator();
+
+const Stack = createStackNavigator();
+
+const BottomTab = createBottomTabNavigator();
+
+function App() {
+  return (
+    //'BottomTab' can be interchanged with 'Stack' or 'Drawer' to display the other Navigator types if desired
+    <NavigationContainer>
+      <BottomTab.Navigator initialRouteName="Home">
+        <BottomTab.Screen name="Home" component={HomeScreen} />
+        <BottomTab.Screen name="Login" component={LoginScreen} />
+        <BottomTab.Screen name="Welcome" component={LandingScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ Styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header:{
+        fontSize: 30,
+        
+    },
+    textField:{
+        fontSize: 20,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 20,
+        width: '80%',
+        padding: 10,
+        margin: 10,
+    },
+});
+
+export default styles;
